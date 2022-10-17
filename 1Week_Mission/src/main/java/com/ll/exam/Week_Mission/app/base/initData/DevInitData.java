880042206d652cb720ed30f6970a2e2512aa2373
Ju Minji <alswl0w0@gmail.com>
2022-10-17 17:12:51 +0900

    chore : additonal setting

---
+++ 1Week_Mission/src/main/java/com/ll/exam/Week_Mission/app/base/initData/DevInitData.java
@@ -0,0 +1,9 @@
+package com.ll.exam.Week_Mission.app.base.initData;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("dev")
+@Configuration
+public class DevInitData {
+}
