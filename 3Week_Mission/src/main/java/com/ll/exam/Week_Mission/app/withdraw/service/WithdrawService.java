3eb150b8f0fb05ab6e17dbc72c38d1ed44c567a3
Ju Minji <alswl0w0@gmail.com>
2022-11-02 11:35:35 +0900

    feat : 출금신청리스트 (#17)

--- 3Week_Mission/src/main/java/com/ll/exam/Week_Mission/app/withdraw/service/WithdrawService.java
+++ 3Week_Mission/src/main/java/com/ll/exam/Week_Mission/app/withdraw/service/WithdrawService.java
@@ -3,9 +3,13 @@ package com.ll.exam.Week_Mission.app.withdraw.service;
 import com.ll.exam.Week_Mission.app.member.entity.Member;
 import com.ll.exam.Week_Mission.app.withdraw.entity.Withdraw;
 import com.ll.exam.Week_Mission.app.withdraw.repository.WithdrawRepository;
+import com.ll.exam.Week_Mission.util.Ut;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Transactional
 @Service
@@ -42,4 +46,14 @@ public class WithdrawService {
         oldWithdraw.setPrice(price);
     }
 
+    public List<Withdraw> findAllByCreateDateBetweenOrderByIdAsc(String yearMonth) {
+        int monthEndDay = Ut.date.getEndDayOf(yearMonth);
+
+        String fromDateStr = yearMonth + "-01 00:00:00.000000";
+        String toDateStr = yearMonth + "-%02d 23:59:59.999999".formatted(monthEndDay);
+        LocalDateTime fromDate = Ut.date.parse(fromDateStr);
+        LocalDateTime toDate = Ut.date.parse(toDateStr);
+
+        return withdrawRepository.findAllByCreateDateBetweenOrderByIdAsc(fromDate, toDate);
+    }
 }
