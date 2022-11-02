269f28e30ffc2d43baacd4db0b7da22185f6a1f3
Ju Minji <alswl0w0@gmail.com>
2022-11-02 14:16:19 +0900

    feat : 출금신청 처리(관리자) (#17)

--- 3Week_Mission/src/main/java/com/ll/exam/Week_Mission/app/withdraw/service/WithdrawService.java
+++ 3Week_Mission/src/main/java/com/ll/exam/Week_Mission/app/withdraw/service/WithdrawService.java
@@ -3,8 +3,10 @@ package com.ll.exam.Week_Mission.app.withdraw.service;
 import com.ll.exam.Week_Mission.app.cash.entity.CashLog;
 import com.ll.exam.Week_Mission.app.cash.entity.EventGroup;
 import com.ll.exam.Week_Mission.app.cash.entity.PayGroup;
+import com.ll.exam.Week_Mission.app.exception.DataNotFoundException;
 import com.ll.exam.Week_Mission.app.member.entity.Member;
 import com.ll.exam.Week_Mission.app.member.service.MemberService;
+import com.ll.exam.Week_Mission.app.rebate.entity.RebateOrderItem;
 import com.ll.exam.Week_Mission.app.withdraw.entity.Withdraw;
 import com.ll.exam.Week_Mission.app.withdraw.repository.WithdrawRepository;
 import com.ll.exam.Week_Mission.util.Ut;
@@ -24,15 +26,12 @@ public class WithdrawService {
 
     public void apply(Member applicant, String bankName, String bankAccountNo, int price, String accountHolder) {
 
-        CashLog withdrawCashLog = memberService.addCash(applicant, -price, EventGroup.WITHDRAW, PayGroup.CASH, null);
-
         Withdraw withdraw = Withdraw.builder()
                 .bankName(bankName)
                 .bankAccountNo(bankAccountNo)
                 .price(price)
                 .accountHolder(accountHolder)
                 .member(applicant)
-                .withdrawCashLog(withdrawCashLog)
                 .build();
 
         withdrawRepository.save(withdraw);
@@ -52,6 +51,19 @@ public class WithdrawService {
     }
 
     public void withdraw(Long withdrawApplyId) {
+        // 1. withdraw 리턴
+        Withdraw withdraw = withdrawRepository.findById(withdrawApplyId).get();
+
+        // 2. null 체크
+        if(withdraw == null){
+            throw new DataNotFoundException("해당 출금 신청 건이 존재하지 않습니다.");
+        }
+
+        // 3. cashLog로 예치금 출금 반영
+        CashLog withdrawCashLog = memberService.addCash(withdraw.getMember(), -withdraw.getPrice(), EventGroup.WITHDRAW, PayGroup.CASH, null);
+
+        // 4. withdraw 출금여부 set
+        withdraw.setWithdrawDone(withdrawCashLog.getId());
     }
 
     public Withdraw findByMemberId(Long applicantId) {
